2ab23e3a911e80647ae5fe0c8ac78b6f1073e7f0
Wallace Neto <wallace.neto@outlook.com>
2024-01-06 12:15:44 -0300

    feat: creating global style and tasklist screen style

--- src/screens/TaskList/TaskList.tsx
+++ src/screens/TaskList/TaskList.tsx
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
 import {Text, View, ImageBackground} from 'react-native';
+import moment from 'moment';
+import '../../../node_modules/moment/locale/pt-br';
 
 import styles from './styles';
 import todayImage from '../../../assets/imgs/today.jpg';
 
 export default class TaskList extends Component {
   render() {
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
+
     return (
       <View style={styles.container}>
-        <ImageBackground source={todayImage} style={styles.background} />
+        <ImageBackground source={todayImage} style={styles.background}>
+          <View style={styles.titleBar}>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>{today}</Text>
+          </View>
+        </ImageBackground>
+
         <View style={styles.taskList}>
-          <Text>Lista de tarefas</Text>
+          <Text>Tarefa #01</Text>
+          <Text>Tarefa #02</Text>
+          <Text>Tarefa #03</Text>
         </View>
       </View>
     );
--- src/screens/TaskList/styles.ts
+++ src/screens/TaskList/styles.ts
@@ -1,5 +1,7 @@
 import {StyleSheet} from 'react-native';
 
+import commonStyles from '../../global/commonStyles';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -7,8 +9,26 @@ const styles = StyleSheet.create({
   background: {
     flex: 3,
   },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
   taskList: {
     flex: 7,
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
   },
 });
 
